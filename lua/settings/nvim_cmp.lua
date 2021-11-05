da94edf3a37a52846084d8752ead25b582159403
benja robles <benjaa.robless@gmail.com>
2021-11-05 15:11:21 -0300

    fix tab autocompletion since change in cmp floating window to native menu

--- lua/settings/nvim_cmp.lua
+++ lua/settings/nvim_cmp.lua
@@ -14,23 +14,15 @@ cmp.setup{
 		['<C-Space>'] = cmp.mapping.complete(),
 		['<C-e>'] = cmp.mapping.close(),
 		['<Tab>'] = function(fallback)
-			if vim.fn.pumvisible() == 1 then
-				vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true, true, true), 'n')
-			elseif luasnip.expand_or_jumpable() then
-				vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', 
-				true, 
-				true, 
-				true), '')
+			if cmp.visible() then
+				cmp.select_next_item()
 			else
 				fallback()
 			end
     end,
     ['<S-Tab>'] = function(fallback)
-		if vim.fn.pumvisible() == 1 then
-			vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-p>', true, true, true), 'n')
-		elseif luasnip.jumpable(-1) then
-			vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', 
-			true, true, true), '')
+		if cmp.visible() then
+			cmp.select_prev_item()
       else
         fallback()
       end
