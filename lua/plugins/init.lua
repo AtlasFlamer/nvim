3d85e836198b5b25f7d877a4d79cbaa1a45b694c
benja robles <benjaa.robless@gmail.com>
2022-02-07 01:58:15 -0300

    add tmux config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,9 +1,30 @@
 vim.cmd 'packadd packer.nvim'
-
 local packer = require('packer')
-local util = require('packer.util')
+-- local util = require('packer.util') hmm not used?
 
 packer.startup(function()
+	use({
+		"aserowy/tmux.nvim",
+		config = function()
+			require("tmux").setup({
+				-- overwrite default configuration
+				-- here, e.g. to enable default bindings
+				copy_sync = {
+					-- enables copy sync and overwrites all register actions to
+					-- sync registers *, +, unnamed, and 0 till 9 from tmux in advance
+					enable = true,
+				},
+				navigation = {
+					-- enables default keybindings (C-hjkl) for normal mode
+					enable_default_keybindings = true,
+				},
+				resize = {
+					-- enables default keybindings (A-hjkl) for normal mode
+					enable_default_keybindings = true,
+				}
+			})
+		end
+	})
 	use {
 		'tikhomirov/vim-glsl',
 		opt = true,
