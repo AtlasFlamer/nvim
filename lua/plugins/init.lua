825be0aef729f8bcc1d574d219f085e2bb36b30d
Your Name <you@example.com>
2022-04-15 12:37:13 -0400

    add kotlin!

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,6 +3,7 @@ local packer = require('packer')
 -- local util = require('packer.util') hmm not used?
 
 packer.startup(function()
+        use 'udalov/kotlin-vim'
         use 'iamcco/markdown-preview.nvim'
 	use({
 		"aserowy/tmux.nvim",
