d8deffe3d74ff936d90c21bbb179e394c4203dd9
Your Name <you@example.com>
2022-08-26 00:13:19 -0400

    sacar configuracion de mdeval

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,19 +1,4 @@
 vim.cmd[[packadd mdeval.nvim]]
-require'mdeval'.setup({
-    require_confirmation = false,
-    eval_options = {
-        cpp = {
-            command = {"g++", "-std=c++20", "-O0"},
-            default_header = [[
-            #include <iostream>
-            #include <vector>
-            ]],
-        },
-        python = {
-            command = {"python"},
-        }
-    }
-});
 vim.g.markdown_fenced_languages = {'python', 'cpp'}
 
 vim.api.nvim_set_keymap('n', '<leader>c', ':MdEval<CR>', {})
