a3f51d8db5531f74fb27c7e270aab5ae096ba432
Your Name <you@example.com>
2022-08-26 00:13:39 -0400

    limpiar main

--- init.lua
+++ init.lua
@@ -9,59 +9,10 @@ filetype plugin on
 autocmd SwapExists * let v:swapchoice = "e"
 ]]
 require ('settings/remap')
+require ('settings/nvim_cmp')
 -- packadd cmp_snip
 require('plugins')
 require ('plugins.bar')
-local cmp = require('cmp')
-local select_opts = {behabior = cmp.SelectBehavior.Select}
 
-local maps = {
-    ["<Tab>"] = cmp.mapping(function(fallback)
-        local col = vim.fn.col('.') - 1
-        if cmp.visible() then
-            cmp.select_next_item(select_opts)
-        elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-            fallback()
-        else
-            cmp.complete()
-        end
-
-        end, {'i', 's'}),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-        local col = vim.fn.col('.') - 1
-        if cmp.visible() then
-            cmp.select_prev_item(select_opts)
-        elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-            fallback()
-        else
-            cmp.complete()
-        end
-
-        end, {'i', 's'}),
-
-         ['<CR>'] = cmp.mapping.confirm({select = true}),
-         ['<Up>'] = cmp.mapping.select_prev_item(select_opts),
-         ['<Down>'] = cmp.mapping.select_next_item(select_opts),
-
-         ['<C-p>'] = cmp.mapping.select_prev_item(select_opts),
-         ['<C-n>'] = cmp.mapping.select_next_item(select_opts),
-         ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-         ['<C-f>'] = cmp.mapping.scroll_docs(4),
-}
-
-
-
-cmp.setup({
-    sources = {
-        {name = 'nvim_lsp'},
-        {name = "luasnip"},
-        {name = "buffer"},
-        {name = "path"}
-    },
-    mapping = maps
-})
-
-require('luasnip')
-require("luasnip.loaders.from_vscode").lazy_load()
 
 
